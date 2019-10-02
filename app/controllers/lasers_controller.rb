102706100cf8a9cfc8528b8867a71f3ec061702e
Morgan VanYperen <morgvanny@gmail.com>
2019-10-02 08:09:33 -0700

    pre-devise stuff

--- app/controllers/lasers_controller.rb
+++ app/controllers/lasers_controller.rb
@@ -1,2 +1,40 @@
 class LasersController < ApplicationController
+
+  def index
+    if params[:robot_id]
+      @lasers = Robot.find(params[:robot_id]).lasers
+    else
+      @lasers = Laser.all
+    end
+  end
+
+  def new
+    if params[:robot_id]
+      @laser = Laser.new(robot_id: params[:robot_id])
+    else
+      @laser = Laser.new
+    end
+  end
+
+  def show
+    @laser = Laser.find(params[:id])
+  end
+  def create
+    @laser = Laser.new(laser_params)
+    binding.pry
+    if @laser.save
+      redirect_to @laser
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def laser_params
+     params.require(:laser).permit(
+      :color,
+      :robot_id,
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
   resources :lasers
-  resources :robots
+  resources :robots, only: [:index, :show, :new] do
+    resources :lasers, only:[:index, :new, :create]
+  end
+
+  #get '/robots/:robot_id/lasers', to: 'lasers#index', as: 'robot_lasers'
+
+  # /artists/:artist_id/songs/:id
+
+  # /artists/:artist_id/songs
+
+  resources :owners, shallow: true do
+    resources :cats do
+      resources :toys
+    end
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
